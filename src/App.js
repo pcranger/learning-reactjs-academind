8e3f3988a23b83f65cc855d6ef1b368fdb26e6d0
pcranger <tmhieu2000@gmail.com>
2020-03-19 15:52:22 +0700

    button that changes Max to Maximilian

--- src/App.js
+++ src/App.js
@@ -8,14 +8,25 @@ class App extends Component{
         {name: 'Max', age: 28},
         {name: 'Manu' ,age : 29},
         {name: 'Steph' ,age : 26}
-      ]
+      ],
+      otherState: 'some other value'
     }
 
+    switchNameHandler  = () =>{
+      // console.log('Was clicked!');
+      // don't change name like this: this.state.persons[0].name = 'Maximilian';
+      this.setState({
+        persons: [
+        {name: 'Maximilian', age: 28},
+        {name: 'Manu' ,age : 29},
+        {name: 'Steph' ,age : 26}
+      ] })
+    }
 
   render() {
     return(
       <div className="App">
-        <button>Switch Name</button>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
         <Person name = {this.state.persons[0].name} age = {this.state.persons[0].age}/>
         <Person name = {this.state.persons[1].name} age = {this.state.persons[1].age}>My Hobbies: Racing</Person>
         <Person name = {this.state.persons[2].name} age = {this.state.persons[2].age}/>
